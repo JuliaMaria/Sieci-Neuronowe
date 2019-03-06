6dd1425b83c947ac769539ebe9a219af22acbe39
JuliaMaria <j.may2501@gmail.com>
2019-03-06 15:10:31 +0100

    Add files via upload

---
+++ LogicalGates.py
@@ -0,0 +1,28 @@
+﻿# May Julia
+# Cw. 1
+# Python
+
+import numpy as np
+
+def notOperator(bit):
+    weights = np.array([-0.5, 0.3])
+    result = np.dot(weights, np.array([bit, 1]))
+    return 1 if result>=0 else 0
+
+def andOperator(bit1, bit2):
+    weights = np.array([0.4, 0.4, -0.6])
+    result = np.dot(weights, np.array([bit1, bit2, 1]))
+    return 1 if result>=0 else 0
+
+def orOperator(bit1, bit2):
+    weights = np.array([0.4, 0.4, -0.3])
+    result = np.dot(weights, np.array([bit1, bit2, 1]))
+    return 1 if result>=0 else 0
+
+def nandOperator(bit1, bit2):
+    weights = np.array([-0.3, -0.3, 0.4])
+    result = np.dot(weights, np.array([bit1, bit2, 1]))
+    return 1 if result>=0 else 0
+
+
+
